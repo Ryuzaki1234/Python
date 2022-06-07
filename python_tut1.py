40770cc06d8bb265f1ec21acd60221954cbfcf15
Ryuzaki1234 <106685890+Ryuzaki1234@users.noreply.github.com>
2022-06-07 13:47:44 +0530

    Create python_tut1.py

---
+++ python_tut1.py
@@ -0,0 +1,30 @@
+# print a string
+# string means letter or alphabet
+f = "Navgun"
+print(f)
+
+# there are five types
+# +, -, /, %, *
+
+# addition
+a = 10 + 1000
+print(a)
+
+# substract
+b = 8 - 9
+print(b)
+
+# divide
+c = 4/2
+print(int(c))
+
+# remainder
+d = 7 % 7
+print(d)
+
+# multiply
+e = 2 * 2
+print(e)
+
+
+# THANKS FOR WATCHING
